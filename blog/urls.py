995a0ff48599c12b36da1d1e6557b453e905e5ff
eosull <osullivaneoin442@gmail.com>
2023-05-03 12:42:37 +0000

    feat:add post detail page and connected home & explore nav links

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('explore', views.ExplorePosts.as_view(), name='explore'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -15,3 +15,21 @@ class ExplorePosts(generic.ListView):
     queryset = Post.objects.order_by('-created_at')
     template_name = 'explore.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *arg, **kwargs):
+        queryset = Post.objects
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_at')
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "all_posts": queryset,
+                "post": post,
+                "comments": comments,
+            },
+        )
