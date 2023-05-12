a0da51a0803d1689d39acf33326aad0b8d124fc8
eosull <osullivaneoin442@gmail.com>
2023-05-12 10:01:49 +0000

    feat:style forms for login/register/new post & edit post

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.order_by('-created_at')
     template_name = 'index.html'
-    paginate_by = 6
+    paginate_by = 3
 
 
 class ExplorePosts(generic.ListView):
