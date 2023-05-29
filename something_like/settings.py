8e436af5296c8ce5140ced62ec1ed9b28e796aff
eosull <osullivaneoin442@gmail.com>
2023-05-29 15:43:36 +0000

    feat:add feedback messages for login/logout

--- something_like/settings.py
+++ something_like/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os
 import dj_database_url
 
@@ -66,6 +67,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
