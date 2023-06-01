6e6a2f71a321c663dcb05e102a6cf6f68cf6ee73
eosull <osullivaneoin442@gmail.com>
2023-06-01 13:53:59 +0000

    refactor:move js script from base template to static dir

--- something_like/settings.py
+++ something_like/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["something-like-pp4.herokuapp.com", '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ["something-like-pp4.herokuapp.com", '8000-eosull-somethinglike-eaqot6kiyaa.ws-eu98.gitpod.io', 'localhost']
 CSRF_TRUSTED_ORIGINS = ["http://something-like-pp4.herokuapp.com",
                         'http://127.0.0.1:8000/',
                         'https://8000-eosull-somethinglike-hrdjmz45bwr.ws-eu96.gitpod.io',
