14505816936df071afcd24fd14f6cf737e21d7e3
eosull <osullivaneoin442@gmail.com>
2023-07-02 17:42:21 +0000

    test:set debug to true to debug heroku error

--- something_like/settings.py
+++ something_like/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Adding Allowed_Hosts and Trusted Origins for development servers
 ALLOWED_HOSTS = ["something-like-pp4.herokuapp.com",
